5228b471af012b8e73f30f90d550e7478d187a39
Utkarsh Sharma <f20160600@goa.bits-pilani.ac.in>
2021-06-29 10:51:12 +0530

    Assignment 4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
@@ -22,14 +22,7 @@ function Expenses(props) {
                     selected={filteredYear} 
                     onChangeFilter={filterChangeHandler} 
                 />
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date} 
-                    />
-                ))}
+                <ExpensesList expenses={filteredExpenses}/>
             </Card>
         </div>
     )
